1e8501649bf2219ef4b7fc6243b6dddac541417a
se-bayan-elian <bezo2024@gmail.com>
2023-07-08 23:38:18 +0300

    edit styled.div

--- src/components/global/container/style.js
+++ src/components/global/container/style.js
@@ -1,6 +1,6 @@
 import { styled } from "styled-components";
 
-export const StyledContainer = styled`
+export const StyledContainer = styled.div`
  margin : auto;
  width : ${props => props.variant =='lg' ? '69.5%':'46.6%'} ;
  @media (max-width : 991px){
