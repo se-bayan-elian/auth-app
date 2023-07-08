94e246224933c449767d158379b1828504de34c1
se-bayan-elian <bezo2024@gmail.com>
2023-07-09 02:46:51 +0300

    update container

--- src/components/global/container/Container.jsx
+++ src/components/global/container/Container.jsx
@@ -3,7 +3,9 @@ import { StyledContainer } from './style'
 
 const Container = ({variant,children}) => {
   return (
-    <StyledContainer className='container' variant={variant}>{children}</StyledContainer>
+    <StyledContainer className='container' variant={variant}>
+      {children}
+    </StyledContainer>
   )
 }
 
