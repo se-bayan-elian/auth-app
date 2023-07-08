9101624afece54bc10d003c70cd4608a54ddcea9
se-bayan-elian <bezo2024@gmail.com>
2023-07-08 23:12:11 +0300

    add container global components

---
+++ src/components/global/container/Container.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyledContainer } from './style'
+
+const Container = ({variant,children}) => {
+  return (
+    <StyledContainer className='container' variant={variant}>{children}</StyledContainer>
+  )
+}
+
+export default Container
---
+++ src/components/global/container/style.js
@@ -0,0 +1,9 @@
+import { styled } from "styled-components";
+
+export const StyledContainer = styled`
+ margin : auto;
+ width : ${props => props.variant =='lg' ? '69.5%':'46.6%'} ;
+ @media (max-width : 991px){
+  width : 95%;
+ }
+`
