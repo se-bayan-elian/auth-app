e3cbbdeeaf633c2ad27801f0c68fc7d064c976c9
se-bayan-elian <bezo2024@gmail.com>
2023-07-08 13:24:10 +0300

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
+  const [page ,setPage] = useState('login')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {page == 'login' ? <Login setPage={setPage}/>: <Register setPage={setPage}/>}
     </div>
-  );
+  )
 }
 
 export default App;
